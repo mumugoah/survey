787277ddb8346eeb10f21f7c86ccc66ff11ce887
mumulife <sunnygaofan@gmail.com>
2024-07-18 22:18:29 +0800

    修改页面

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -112,7 +112,7 @@ export default defineConfig({
     port: 8080,
     proxy: {
       '/api': {
-        target: 'http://127.0.0.1:3000',
+        target: 'http://ahdx-frp.vip:5661',
         changeOrigin: true
       }
     }
